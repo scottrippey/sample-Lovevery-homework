266384e3bd146554d2e856238401a338b87fbf9f
scottrippey <scott.william.rippey@gmail.com>
2020-09-15 15:32:07 -0600

    [header] Added more test assertions

--- src/components/MessagesList.test.tsx
+++ src/components/MessagesList.test.tsx
@@ -3,14 +3,26 @@ import { render } from '@testing-library/react';
 import { MessagesList } from "./MessagesList";
 
 import { enableMockAdapter } from "~/common/messagesClient.mock";
+import {StatusReporterProvider} from "~/components/StatusReporter";
 
-enableMockAdapter({ delayResponse: 0 });
+enableMockAdapter({ delayResponse: 100 });
 
-describe("MessagesList", function () {
-  it("should render an empty list", async () => {
-    const el = render(<MessagesList />);
+describe("MessagesList", () => {
+  const wrapper = ({ children }) => {
+    return <StatusReporterProvider> { children } </StatusReporterProvider>;
+  };
+  it("should render a message list with mock entries", async () => {
+    const el = render(<MessagesList />, { wrapper });
 
-    await expect(el.findByText("No messages to display")).resolves.toBeDefined();
+    // Ensure the messages were displayed correctly:
+    await expect(el.findByText("Scott")).resolves.toBeDefined();
+    await expect(el.findByText("I love mocks")).resolves.toBeDefined();
+    await expect(el.findByText("I love socks")).resolves.toBeDefined();
+
+    await expect(el.findByText("Jim")).resolves.toBeDefined();
+    await expect(el.findByText("Question: what kind of bear is best?")).resolves.toBeDefined();
+    await expect(el.findByText("Fact: bears love beets")).resolves.toBeDefined();
+
   });
 
   describe("add", () => {
