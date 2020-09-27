4676c7545841e42c2b849bc6374865112b29d300
scottrippey <scott.william.rippey@gmail.com>
2020-09-27 13:46:36 -0600

    [messages] Improved code quality of StatusReporter, based on feedback

--- src/components/StatusReporter.tsx
+++ src/components/StatusReporter.tsx
@@ -3,30 +3,31 @@ import React from "react";
 // @ts-ignore
 const StatusReporterContext = React.createContext<StatusReporter>(null);
 
-export const StatusReporterProvider = ({ children }) => {
-  const statusAPI = useCreateStatusReporter();
-  return <StatusReporterContext.Provider value={statusAPI}>{children}</StatusReporterContext.Provider>;
-};
-
-type StatusReporter = ReturnType<typeof useCreateStatusReporter>;
-
-function useCreateStatusReporter() {
+/**
+ * Creates and provides the StatusReporter API
+ *
+ * The StatusReporter is used to show a status, or error, in the header.
+ */
+export function StatusReporterProvider({ children }) {
   const [status, setStatus] = React.useState<JSX.Element | null>(null);
-  const statusReporter = React.useMemo(
-    () => ({
-      status,
-      setStatus,
-      clearStatus() {
-        setStatus(null);
-      },
-    }),
-    [status]
-  );
-  return statusReporter;
+  const statusReporter: StatusReporter = {
+    status,
+    setStatus,
+    clearStatus() {
+      setStatus(null);
+    },
+  };
+  return <StatusReporterContext.Provider value={statusReporter}>{children}</StatusReporterContext.Provider>;
+}
+
+interface StatusReporter {
+  status: JSX.Element | null;
+  setStatus(status: JSX.Element | null): void;
+  clearStatus(): void;
 }
 
 /**
- * Retrieves the StatusReporter
+ * Retrieves the StatusReporter API
  */
 export function useStatusReporter() {
   return React.useContext(StatusReporterContext);
